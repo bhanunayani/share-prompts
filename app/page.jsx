eae2e0311cd8400b9b224cfd1dfe9b5e0c24cd97
bhanunayani <bhanunayani@gmail.com>
2024-06-28 15:20:07 -0400

    home page heading

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+const home = () => {
+  return (
+    <section className="w-full flex-center">
+        <h1 className="head_text text-center">
+            Discover & Share
+        </h1>
+    </section>
+  )
+}
+
+export default home
