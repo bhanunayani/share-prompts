7e72ba1c35a6003720d7e7b897a2e64a67632ba9
bhanunayani <bhanunayani@gmail.com>
2024-06-30 11:34:11 -0400

    succesfully connected to database

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -22,7 +22,7 @@ const Nav = () => {
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href='/' className="flex gap-2 flex-center">
       <Image src="/assets/images/logo.svg" width={30} height={30} alt="Promptopia logo" className="object-contain"/>
-      <p className="logo_text">Promptopia</p>
+      <p className="logo_text">Share Prompt</p>
       </Link>
 
       {/*desktop navigation*/}
